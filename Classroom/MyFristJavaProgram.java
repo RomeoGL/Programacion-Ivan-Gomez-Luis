d8c9f899047c10abff5c6e6fa59ae65945d00603
RomeoGL <lepre_123@hotmail.com>
2018-01-22 09:34:22 -0600

    New Version

--- Classroom/MyFristJavaProgram.java
+++ Classroom/MyFristJavaProgram.java
@@ -1,8 +1,11 @@
 /***********************
-* Author: Ivan Gomez Luis
-* Program: MyFristJavaProgram.java
-* Description: This program print Hello world
-*
+Author: Ivan Gomez Luis
+Program: MyFristJavaProgram.java
+Description: This program print Hello world
+Editado
+What's up!!!!
+What are you foing??
+I am learning how to use GIT
 ***********************/
 public class MyFristJavaProgram
 {
@@ -10,5 +13,7 @@ public class MyFristJavaProgram
   {
     System.out.println("Hello World");
     System.out.printli("Posicion 0:"+args[0]);
+    System.out.printli("Posicion 0:"+args[1]);
+    System.out.printli("Posicion 0:"+args[2]);
   }
 }
