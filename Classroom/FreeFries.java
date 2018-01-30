a7ecaffee086edc68cbd76810a23fbb399e0eb3c
RomeoGL <lepre_123@hotmail.com>
2018-01-29 23:30:30 -0600

    January 29

---
+++ Classroom/FreeFries.java
@@ -0,0 +1,24 @@
+/****
+FreeFries.java
+Ivan Gomez Luis
+This program reads points score by home team adn the opposing team and determinates
+IF the fans win free french fries
+****/
+import javax.swing.JOptionPane; //para utilizar modo grafico GUI
+public class FreeFries
+{
+  public static void main(String[]args)
+  {
+    int homePoints; //points scored by home team
+    int opponentPoints; //points scored by opposing team
+    homePoints =
+    Integer.parseInt(JOptionPane.showInputDialog("Enter home points"));
+    opponentPoints =
+    Integer.parseInt(JOptionPane.showInputDialog("Enter opponent points"));
+    if (homePoints>opponentPoints && homePoints >= 100)
+    {
+      JOptionPane.showMessageDialog(null, "A free order of French Fries for White Tigers Fans");
+
+    }
+  }
+}
