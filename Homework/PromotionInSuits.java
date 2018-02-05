650a5cbdbae00804deb4913577c08b6042734210
RomeoGL <lepre_123@hotmail.com>
2018-02-04 23:28:37 -0600

    Homework

---
+++ Homework/PromotionInSuits.java
@@ -0,0 +1,30 @@
+/*
+Ivan Gomez Luis
+PromotionInSuits.java
+This program calculates how much a customer will pay for a suit to which a discount has been applied
+exercise 2.4
+*/
+import javax.swing.JOptionPane;
+public class PromotionInSuits
+{
+  public static void main(String[] args)
+  {
+    double ct; //costo del traje
+    double de; //descuento que se obtendra
+    double pf; //Precio final del traje
+    ct=Double.parseDouble(JOptionPane.showInputDialog("Enter the price of suit"));
+    if (ct>=2500)
+    {
+      de=(ct*0.15);
+      pf=(ct-de);
+      JOptionPane.showMessageDialog(null, "Suit \n\n" + "Price of suit: $" + ct + "\nDiscount: $" + de + "\nFinal price: $" + pf );
+    }
+    else if (ct<2500)
+    {
+      de=(ct*0.8);
+      pf=(ct-de);
+      JOptionPane.showMessageDialog(null, "Suit \n\n" + "Price of suit: $" + ct + "\nDiscount: $" + de + "\nFinal price: $" + pf );
+    }
+
+  }
+}
---
+++ Homework/SaleOfPencils.java
@@ -0,0 +1,26 @@
+/*
+Ivan Gomez Luis
+SaleOfPencils.java
+This program calculates when you should pay for a purchase of pencils
+Exercise 2.3
+*/
+import javax.swing.JOptionPane;
+public class SaleOfPencils
+{
+  public static void main(String[] args)
+  {
+    int x; //Cantidad de lapices
+    double pag; //Pago que se realizar por los lapices
+    x=Integer.parseInt(JOptionPane.showInputDialog("Enter the number of pencils purchased"));
+    if (x>1000)
+    {
+      pag=(x*0.85);
+      JOptionPane.showMessageDialog(null, "The total to pay for the " + x + " pencils is $" + pag );
+    }
+    else if (x<=1000)
+    {
+      pag=(x*0.90);
+      JOptionPane.showMessageDialog(null, "The total to pay for the " + x + " pencils is $" + pag );
+    }
+  }
+}
