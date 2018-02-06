d292e48159b3270181cfe61bb7b6465b836ebce6
RomeoGL <lepre_123@hotmail.com>
2018-02-06 07:55:29 -0600

    February 6

---
+++ Classroom/WhileLoop.java
@@ -0,0 +1,24 @@
+/*Ivan Gomez Luis
+WhileLoop.java
+This program makes entries in a bridal registry
+*/
+import javax.swing.JOptionPane;
+public class WhileLoop
+{
+  public static void main(String[] args)
+  {
+    String registry="";
+    char more;
+    more=JOptionPane.showInputDialog("Do you want to create a bridal registry? (Y/N)").charAt(0);
+    while(more=='Y')
+    {
+      registry += JOptionPane.showInputDialog("Enter item: ");
+      registry += JOptionPane.showInputDialog("Enter store: \n");
+      more=JOptionPane.showInputDialog("Any more items? (Y/N)").charAt(0);
+    }//end while
+    if (!registry.equals(""))
+  {
+    JOptionPane.showMessageDialog(null, "\nBridal registry\n + registry");
+  }
+  }//
+}//end program
