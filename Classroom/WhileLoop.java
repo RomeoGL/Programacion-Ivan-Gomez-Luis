aa1e45f4a40ff2b85e4692bef4d778cc83d2f497
RomeoGL <lepre_123@hotmail.com>
2018-02-06 12:25:56 -0600

    February 6

--- Classroom/WhileLoop.java
+++ Classroom/WhileLoop.java
@@ -10,15 +10,16 @@ public class WhileLoop
     String registry="";
     char more;
     more=JOptionPane.showInputDialog("Do you want to create a bridal registry? (Y/N)").charAt(0);
-    while(more=='Y')
+    while(more=='Y' || more 'y')
     {
       registry += JOptionPane.showInputDialog("Enter item: ");
       registry += JOptionPane.showInputDialog("Enter store: \n");
+      registry +="\n";
       more=JOptionPane.showInputDialog("Any more items? (Y/N)").charAt(0);
     }//end while
     if (!registry.equals(""))
   {
-    JOptionPane.showMessageDialog(null, "\nBridal registry\n + registry");
+    JOptionPane.showMessageDialog(null, "\nBridal registry\n" + registry);
   }
   }//
 }//end program
