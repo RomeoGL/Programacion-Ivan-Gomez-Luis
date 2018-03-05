860e76d864d3bd5ef2c3104799f4861c585b1afe
RomeoGL <lepre_123@hotmail.com>
2018-03-04 23:55:14 -0600

    February 26

--- Classroom/Circle.java
+++ Classroom/Circle.java
@@ -5,10 +5,19 @@ This class calculates the area of a circle
 */
 import javax.swing.*;
 
-public class circle
+public class Circle
 {
   //prupies
   private double radius; //Se teriere que pertenece a
+  //Constructor
+  /*public circle()
+  {
+  }*/
+  //Constructor curcke iverkiad
+  public Circle(double radius)
+  {
+    this.radius=radius;
+  }
   //methods
   public void setRadius(double radius)
   {
@@ -17,15 +26,15 @@ public class circle
   public double getRadius()
   {
     return this.radius;
-
   }
   public double getArea()
   {
     return Math.pow(this.radius, 2)*Math.PI;
   }
-  public static void main(String[] args) {
-    circle =new circle();
-    circle.setRadius(3.14);
-    JOptionPane.showMessageDialog(null, circle.getArea());
+  public static void main(String[] args)
+  {
+    double radius=Double.parseDouble(JOptionPane.showInputDialog("Enter the radius"));
+    Circle circle=new Circle(radius);
+    JOptionPane.showMessageDialog(null, "The circle's area with a radius of " + circle.getRadius() + " is " + circle.getArea());
   }
 }
